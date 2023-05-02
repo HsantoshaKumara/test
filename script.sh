3b5eb5dfd66c89905679707caa54421bfd565a43
HsantoshaKumara <111377476+HsantoshaKumara@users.noreply.github.com>
2023-05-02 22:10:25 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -6,7 +6,7 @@ cd TEMP
 #cd test.git
 branch_commit_ID="$1"
 #if ! git rev-parse --verify "$branch_commit_ID" >/dev/null 2>&1 
-if ! git ls-remote "https://github.com/HsantoshaKumara/test" | grep -q "$branch_commit_ID"
+if ! git ls-remote "$https://github.com/HsantoshaKumara/test.git" | grep -q "$branch_commit_ID"
 then echo "branch_commit_ID is not valid"
 exit 1
 fi
