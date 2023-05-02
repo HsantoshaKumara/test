390533e7ab41e61d1acba1f419182633d2b4b77d
HsantoshaKumara <111377476+HsantoshaKumara@users.noreply.github.com>
2023-05-02 23:07:17 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,23 +1,66 @@
 #!/bin/bash
+
+# Clone the repository
 rm -rf TEMP
 mkdir TEMP
 cd TEMP
-#git clone --mirror git@github.com:HsantoshaKumara/test.git
-#cd test.git
-branch_commit_ID="$1"
-#if ! git rev-parse --verify "$branch_commit_ID" >/dev/null 2>&1 
-#if ! git ls-remote "$https://github.com/HsantoshaKumara/test.git" | grep -q "$branch_commit_ID"
-#then echo "branch_commit_ID is not valid"
-#exit 1
-
-git clone --mirror https://github.com/HsantoshaKumara/test.git
+GIT_TERMINAL_PROMPT=0 git clone https://${{ secrets.SECRET }}@github.com/HsantoshaKumara/test.git
 cd test.git
+
+# Get the branch or commit ID from user input
 branch_commit_ID="$1"
-if ! git ls-remote "https://${SECRETS}@github.com/HsantoshaKumara/test.git" | grep -q "$branch_commit_ID"
- then
-  echo "branch_commit_ID is not valid"
+if ! git rev-parse --verify "$branch_commit_ID" >/dev/null 2>&1; then
+  echo "Invalid branch or commit ID: $branch_commit_ID"
   exit 1
 fi
+
+# Ask user if they want to rename the tag
+ans="$2"
+while [ "$ans" != "yes" ] && [ "$ans" != "no" ]; do 
+  echo "Please answer with yes or no"
+  read ans
+done
+
+# Rename the tag if requested by the user
+if [ "$ans" == "yes" ]; then
+  suffix=$(date +"%Y-%m-%d_%H.%M.%S")
+  TEMP="PROD-$suffix"
+  while git rev-parse --quiet --verify "refs/tags/$TEMP" >/dev/null; do 
+    echo "Error: $TEMP already exists"
+    exit 1
+  done
+  echo "Renaming tag to $TEMP"
+  git checkout PROD
+  git tag -f $TEMP PROD^{}
+  git tag -d PROD
+  git push origin $TEMP
+  git checkout $branch_commit_ID
+fi
+
+# Checkout the branch or commit ID
+cd ..
+git clone https://${{ secrets.SECRET }}@github.com/HsantoshaKumara/test.git
+cd test
+git checkout $branch_commit_ID
+
+# Create or update the PROD tag
+if [ "$ans" == "yes" ]; then
+  git checkout PROD
+  git tag -d PROD
+  git tag PROD -m "new tag"
+  git push origin :refs/tags/PROD
+  git push origin PROD
+  echo "PROD tag updated"
+else
+  git tag -a PROD -m "new tag"
+  git push origin PROD
+  echo "PROD tag created"
+fi
+
+# Clean up
+cd ../..
+rm -rf TEMP
+
 
 
 
